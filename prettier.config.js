6758064ff50273545db85e836d08d4bf0e3f64bc
Hubert Warchoł <n3tr00x99@gmail.com>
2024-10-03 02:32:21 +0200

    chore: update prettier configuration

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 export default {
-	endOfLine: "auto",
-	printWidth: 110,
+	endOfLine: 'auto',
+	printWidth: 100,
 	tabWidth: 2,
 	useTabs: true,
 	singleQuote: true,
-	arrowParens: "avoid",
+	arrowParens: 'avoid',
 };
