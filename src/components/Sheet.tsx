a7d40f97cb06d50eddf580bedf62760318e249d6
Hubert Warchoł <n3tr00x99@gmail.com>
2025-01-27 02:25:56 +0100

    style: ensure calendar cards have equal height

--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -33,49 +33,58 @@ export function Sheet({ selectedDate }: { selectedDate: Date }) {
 	const { onDateChange } = useDate();
 	const sheet = generateCalendarSheet(startOfMonth(selectedDate));
 
+	const rowCount = Math.ceil(sheet.length / 7);
+
 	return (
-		<Grid templateColumns="repeat(7, 1fr)">
-			{WEEK_DAYS.map(day => (
-				<GridItem
-					key={day}
-					color={day === 'sunday' ? 'red.700' : undefined}
-					textAlign="center"
-					fontWeight="bold"
-					fontSize="xs"
-				>
-					{day.toUpperCase().slice(0, 1)}
-				</GridItem>
-			))}
-			{sheet.map(date => (
-				<GridItem
-					key={date.toString()}
-					minHeight={16}
-					borderTop="2px solid"
-					borderColor={isSameDay(date, selectedDate) ? 'blue.solid' : 'gray.muted'}
-					color={isSunday(date) ? 'red.700' : undefined}
-					textAlign="center"
-					fontSize="xs"
-					// onClick={() => setSelectedDate(date)}
-					onClick={() => {
-						onDateChange(setHours(date, new Date().getHours()));
-					}}
-				>
-					<chakra.h2
-						fontWeight="semibold"
-						opacity={isSameMonth(date, selectedDate) ? 1 : 0.4}
-						my={2}
+		<>
+			<Grid templateColumns="repeat(7, 1fr)">
+				{WEEK_DAYS.map(day => (
+					<GridItem
+						key={day}
+						color={day === 'sunday' ? 'red.700' : undefined}
+						textAlign="center"
+						fontWeight="bold"
+						fontSize="xs"
 					>
-						<chakra.span
-							px={2}
-							py={1}
-							rounded="md"
-							bgColor={isToday(date) ? 'blue.solid' : undefined}
+						{day.toUpperCase().slice(0, 1)}
+					</GridItem>
+				))}
+			</Grid>
+			<Grid
+				templateColumns="repeat(7, 1fr)"
+				templateRows={`repeat(${rowCount}, 1fr)`} // Ensure consistent height for rows
+				height={80}
+			>
+				{sheet.map(date => (
+					<GridItem
+						key={date.toString()}
+						borderTop="2px solid"
+						borderColor={isSameDay(date, selectedDate) ? 'blue.solid' : 'gray.muted'}
+						color={isSunday(date) ? 'red.700' : undefined}
+						textAlign="center"
+						fontSize="xs"
+						// onClick={() => setSelectedDate(date)}
+						onClick={() => {
+							onDateChange(setHours(date, new Date().getHours()));
+						}}
+					>
+						<chakra.h2
+							fontWeight="semibold"
+							opacity={isSameMonth(date, selectedDate) ? 1 : 0.4}
+							my={2}
 						>
-							{getDate(date)}
-						</chakra.span>
-					</chakra.h2>
-				</GridItem>
-			))}
-		</Grid>
+							<chakra.span
+								px={2}
+								py={1}
+								rounded="md"
+								bgColor={isToday(date) ? 'blue.solid' : undefined}
+							>
+								{getDate(date)}
+							</chakra.span>
+						</chakra.h2>
+					</GridItem>
+				))}
+			</Grid>
+		</>
 	);
 }
