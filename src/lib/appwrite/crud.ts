3e667885fead8c5c081c5d90ae45da8dba7fe8a4
Hubert Warchoł <n3tr00x99@gmail.com>
2025-01-28 23:56:30 +0100

    feat: add new actions to getting and adding events

--- src/lib/appwrite/crud.ts
+++ src/lib/appwrite/crud.ts
@@ -1,7 +1,7 @@
-import { ID } from 'appwrite';
+import { ID, Query } from 'appwrite';
 
-import { config, databases } from '@/lib/appwrite';
-import { INewUser } from '@/types/appwrite';
+import { account, config, databases } from '@/lib/appwrite';
+import { INewUser, NewEvent } from '@/types/appwrite';
 
 export async function saveNewUser(user: INewUser) {
 	try {
@@ -15,6 +15,56 @@ export async function saveNewUser(user: INewUser) {
 		return newUser;
 	} catch (error) {
 		console.error('saveNewUser', error);
+		return error;
+	}
+}
+
+export async function getEvents(date: string) {
+	try {
+		const currentAccount = await account.get();
+
+		if (!currentAccount) {
+			throw new Error('No logged user.');
+		}
+
+		console.log('get events', new Date(date).toISOString());
+
+		const events = await databases.listDocuments(config.databaseId, config.eventsCollectionId, [
+			Query.equal('accountId', currentAccount.$id),
+			Query.and([
+				Query.lessThanEqual('startDate', new Date(date).toISOString()),
+				Query.greaterThanEqual('endDate', new Date(date).toISOString()),
+			]),
+		]);
+
+		return events;
+	} catch (error) {
+		console.error('getEvents');
+		return error;
+	}
+}
+
+export async function addNewEvent(event: NewEvent) {
+	try {
+		const processedDates = {
+			startDate: new Date(event.startDate).toISOString(),
+			endDate: new Date(event.endDate).toISOString(),
+		};
+
+		const newEvent = {
+			...event,
+			...processedDates,
+			repeat: event.repeat[0],
+		};
+
+		await databases.createDocument(
+			config.databaseId,
+			config.eventsCollectionId,
+			ID.unique(),
+			newEvent,
+		);
+	} catch (error) {
+		console.error('addNewEvent', error);
 		return error;
 	}
 }
