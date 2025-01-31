bae53d290373f421cefdd08ce191ae5592480ad6
Hubert Warchoł <n3tr00x99@gmail.com>
2025-01-31 16:23:52 +0100

    feat: add events collection id and storage id to config

--- src/lib/appwrite/index.ts
+++ src/lib/appwrite/index.ts
@@ -1,12 +1,15 @@
-import { Account, Client, Databases } from 'appwrite';
+import { Account, Client, Databases, Storage } from 'appwrite';
 
 export const config = {
 	url: import.meta.env.VITE_APPWRITE_URL,
 	projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
 	databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
 	usersCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+	eventsCollectionId: import.meta.env.VITE_APPWRITE_EVENTS_COLLECTION_ID,
+	storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
 };
 
 const client = new Client().setEndpoint(config.url).setProject(config.projectId);
 export const account = new Account(client);
 export const databases = new Databases(client);
+export const storage = new Storage(client);
