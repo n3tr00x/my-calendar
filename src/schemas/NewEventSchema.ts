ecdaa0ee553438204910ecf1602957b321f96249
Hubert Warchoł <n3tr00x99@gmail.com>
2025-01-31 16:12:33 +0100

    feat: create new event schema

---
+++ src/schemas/NewEventSchema.ts
@@ -0,0 +1,39 @@
+import { z } from 'zod';
+
+const validateDateOrder = (startDate: string, endDate: string) => {
+	return new Date(endDate) >= new Date(startDate);
+};
+
+const validateTimeOrder = (startTime: string | null, endTime: string | null) => {
+	if (!startTime || !endTime) {
+		return true;
+	}
+
+	const [startMinutes, startSeconds] = startTime.split(':').map(Number);
+	const [endMinutes, endSeconds] = endTime.split(':').map(Number);
+
+	const startTotalSeconds = startMinutes * 60 + startSeconds;
+	const endTotalSeconds = endMinutes * 60 + endSeconds;
+
+	return endTotalSeconds >= startTotalSeconds;
+};
+
+export const NewEventSchema = z
+	.object({
+		title: z.string().min(1, 'Title is required').trim(),
+		isAllDay: z.boolean(),
+		startDate: z.string(),
+		endDate: z.string(),
+		startTime: z.string().nullable(),
+		endTime: z.string().nullable(),
+		location: z.string().nullable(),
+		repeat: z.array(z.enum(['no-repeat', 'daily', 'weekly', 'monthly'])),
+	})
+	.refine(event => validateDateOrder(event.startDate, event.endDate), {
+		message: 'End date cannot be earlier than start date',
+		path: ['endDate'],
+	})
+	.refine(event => validateTimeOrder(event.startTime, event.endTime), {
+		message: 'End time cannot be earlier than start time',
+		path: ['endTime'],
+	});
