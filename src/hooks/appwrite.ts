58e2886ed5ebaf13510a10768f5240f2ae69a2c9
Hubert Warchoł <n3tr00x99@gmail.com>
2025-02-14 01:15:25 +0100

    feat: create queries to getting events by name and modify action of getting events

--- src/hooks/appwrite.ts
+++ src/hooks/appwrite.ts
@@ -1,8 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { addMonths, subMonths } from 'date-fns';
 
 import { createAccount, signInAccount, signOutAccount } from '@/lib/appwrite/auth';
-import { addNewEvent, getEvents } from '@/lib/appwrite/crud';
+import { addNewEvent, getEvents, getEventsByEventName } from '@/lib/appwrite/crud';
 import { INewAccount, ISignInAccount, NewEvent } from '@/types/appwrite';
+import { formatDateToMonthYear } from '@/utilities/date';
 
 export function useCreateUserAccount() {
 	return useMutation({
@@ -23,11 +25,23 @@ export function useSignOutAccount() {
 }
 
 export function useEvents(date: Date) {
+	const formattedDate = formatDateToMonthYear(date);
+	const prevMonthDate = formatDateToMonthYear(subMonths(date, 1));
+	const nextMonthDate = formatDateToMonthYear(addMonths(date, 1));
+
 	return useQuery({
-		queryKey: ['events', date],
+		queryKey: ['events', prevMonthDate, formattedDate, nextMonthDate],
 		queryFn: () => getEvents(date),
 		staleTime: Infinity,
 		gcTime: 1000 * 60 * 60, // 1h,
+	});
+}
+
+export function useSearchedEvents(eventName: string) {
+	return useQuery({
+		queryKey: ['events', 'search', eventName],
+		queryFn: () => getEventsByEventName(eventName),
+		enabled: !!eventName,
 	});
 }
 
--- src/lib/appwrite/crud.ts
+++ src/lib/appwrite/crud.ts
@@ -1,8 +1,8 @@
 import { ID, Query } from 'appwrite';
+import { addMonths, endOfDay, endOfMonth, startOfDay, startOfMonth, subMonths } from 'date-fns';
 
 import { account, config, databases } from '@/lib/appwrite';
 import { Event, INewUser, NewEvent } from '@/types/appwrite';
-import { formatDateToYearMonthDay } from '@/utilities/date';
 
 export async function saveNewUser(user: INewUser) {
 	return await databases.createDocument(
@@ -14,6 +14,11 @@ export async function saveNewUser(user: INewUser) {
 }
 
 export async function getEvents(date: Date) {
+	const prevMonth = subMonths(date, 1);
+	const nextMonth = addMonths(date, 1);
+	const minDate = startOfMonth(prevMonth);
+	const maxDate = endOfMonth(nextMonth);
+
 	const currentAccount = await account.get();
 
 	if (!currentAccount) {
@@ -23,8 +28,8 @@ export async function getEvents(date: Date) {
 	const events = await databases.listDocuments(config.databaseId, config.eventsCollectionId, [
 		Query.equal('accountId', currentAccount.$id),
 		Query.and([
-			Query.lessThanEqual('startDate', formatDateToYearMonthDay(date)),
-			Query.greaterThanEqual('endDate', formatDateToYearMonthDay(date)),
+			Query.greaterThanEqual('startDate', minDate.toISOString()),
+			Query.lessThanEqual('endDate', maxDate.toISOString()),
 		]),
 	]);
 
@@ -34,6 +39,8 @@ export async function getEvents(date: Date) {
 export async function addNewEvent(event: NewEvent) {
 	const newEvent = {
 		...event,
+		startDate: startOfDay(event.startDate),
+		endDate: endOfDay(event.endDate),
 		repeat: event.repeat[0],
 	};
 
@@ -43,4 +50,16 @@ export async function addNewEvent(event: NewEvent) {
 		ID.unique(),
 		newEvent,
 	);
+}
+
+export async function getEventsByEventName(eventName: string) {
+	const foundEvents = await databases.listDocuments(config.databaseId, config.eventsCollectionId, [
+		Query.contains('title', eventName),
+	]);
+
+	if (!foundEvents) {
+		throw new Error('No found events.');
+	}
+
+	return foundEvents.documents as Event[];
 }
