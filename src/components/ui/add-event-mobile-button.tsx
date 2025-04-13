5884ca8f929e0584dde7692ecaebabfffd079442
Hubert Warchoł <n3tr00x99@gmail.com>
2025-04-14 01:37:48 +0200

    feat: move add event mobile button and topbar

---
+++ src/components/ui/add-event-mobile-button.tsx
@@ -0,0 +1,19 @@
+import { RefAttributes } from 'react';
+import { Button, ButtonProps } from '@chakra-ui/react';
+import { PlusIcon } from 'lucide-react';
+
+export function AddEventMobileButton(props: ButtonProps & RefAttributes<HTMLButtonElement>) {
+	return (
+		<Button
+			variant="subtle"
+			colorPalette="blue"
+			alignSelf="end"
+			rounded="full"
+			size="xl"
+			aspectRatio="square"
+			{...props}
+		>
+			<PlusIcon />
+		</Button>
+	);
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,16 @@
-import { RefAttributes } from 'react';
-import { Button, ButtonProps, Flex, Separator } from '@chakra-ui/react';
-import { PlusIcon } from 'lucide-react';
+import { Flex, Separator } from '@chakra-ui/react';
 
 import { AddEventMobileInput } from '@/components/AddEventMobileInput';
 import { Calendar } from '@/components/Calendar';
 import { Events } from '@/components/Events';
 import { NewEventModal } from '@/components/NewEvent';
+import { Topbar } from '@/components/Topbar';
+import { AddEventMobileButton } from '@/components/ui/add-event-mobile-button';
 
-export function AddEventMobileButton(props: ButtonProps & RefAttributes<HTMLButtonElement>) {
-	return (
-		<Button
-			variant="subtle"
-			colorPalette="blue"
-			alignSelf="end"
-			rounded="full"
-			size="xl"
-			aspectRatio="square"
-			{...props}
-		>
-			<PlusIcon />
-		</Button>
-	);
-}
-
-export function Home() {
+export function HomePage() {
 	return (
 		<Flex as="main" h="100vh" flexDirection="column" mx={2}>
+			<Topbar />
 			<Calendar />
 			<Separator />
 			<Events />
