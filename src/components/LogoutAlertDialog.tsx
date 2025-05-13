547490aea910d0fda7b7f35d64e0dfe5d1142063
Hubert Warchoł <n3tr00x99@gmail.com>
2025-05-13 17:28:30 +0200

    feat: create logout alert dialog

---
+++ src/components/LogoutAlertDialog.tsx
@@ -0,0 +1,41 @@
+import { ReactElement, RefAttributes } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ButtonProps } from '@chakra-ui/react';
+
+import { AlertDialog } from '@/components/AlertDialog';
+import { toaster } from '@/components/ui/toaster';
+import { useSignOutAccount } from '@/hooks/appwrite';
+import { useAuth } from '@/hooks/useAuth';
+
+type LogoutAlertDialogProps = {
+	trigger: ReactElement<ButtonProps & RefAttributes<HTMLButtonElement>>;
+};
+
+export function LogoutAlertDialog({ trigger }: LogoutAlertDialogProps) {
+	const navigate = useNavigate();
+	const { removeAuthentication } = useAuth();
+	const { mutateAsync: signOut } = useSignOutAccount();
+
+	const signOutHandler = async () => {
+		await signOut();
+		removeAuthentication();
+		navigate('/sign-in', { replace: true });
+
+		toaster.create({
+			title: 'You have been logged out successfully.',
+			type: 'success',
+			placement: 'bottom-end',
+			duration: 4000,
+		});
+	};
+
+	return (
+		<AlertDialog
+			trigger={trigger}
+			action={signOutHandler}
+			actionButtonLabel="Log out"
+			title="Leaving your account"
+			description="Your session will be ended. Do you want to continue?"
+		/>
+	);
+}
