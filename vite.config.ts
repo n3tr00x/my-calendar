6e757f9010304423c0246e12e514c551a5a319e4
Hubert Warchoł <n3tr00x99@gmail.com>
2025-05-13 17:46:42 +0200

    feat: expose app metadata (name, version, author) from package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
+import pkg from './package.json';
+
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
 		},
+	},
+	define: {
+		__APP_NAME__: JSON.stringify(pkg.name),
+		__APP_VERSION__: JSON.stringify(pkg.version),
+		__APP_AUTHOR__: JSON.stringify(pkg.author),
 	},
 });
