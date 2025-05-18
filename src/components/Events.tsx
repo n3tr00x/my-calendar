c493242c90be7afcbd0292e1f3b00fc018eeb719
Hubert Warchoł <n3tr00x99@gmail.com>
2025-05-18 02:11:09 +0200

    feat: implement skeleton for event list

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -2,6 +2,7 @@ import { Box, Text } from '@chakra-ui/react';
 
 import { EmptyEventList } from '@/components/EmptyEventList';
 import { EventList } from '@/components/EventList';
+import { EventListSkeleton } from '@/components/EventListSkeleton';
 import { useEvents } from '@/hooks/appwrite';
 import { useSelectedDate } from '@/store/date';
 import { formatShortDate } from '@/utilities/date';
@@ -11,20 +12,18 @@ export function Events() {
 	console.log('<EventList /> render.');
 
 	const selectedDate = useSelectedDate();
-	const { data: events = [], isLoading: isEventLoading } = useEvents(selectedDate);
-
-	if (isEventLoading) {
-		return <p>Loading...</p>;
-	}
+	const { data: events = [], isLoading } = useEvents(selectedDate);
 
 	const eventsWithRecurrence = filterEventsWithRecurrence(selectedDate, events);
 
 	return (
-		<Box as="section" id="events" mx={3} my={2}>
+		<Box as="section" id="events" mx={3} my={2} display={{ base: 'block', lg: 'none' }}>
 			<Text fontSize="xs" color="gray" fontFamily="heading">
 				<span>{formatShortDate(selectedDate)}</span>
 			</Text>
-			{!eventsWithRecurrence || eventsWithRecurrence.length === 0 ? (
+			{isLoading ? (
+				<EventListSkeleton />
+			) : !eventsWithRecurrence || eventsWithRecurrence.length === 0 ? (
 				<EmptyEventList />
 			) : (
 				<EventList events={eventsWithRecurrence} />
