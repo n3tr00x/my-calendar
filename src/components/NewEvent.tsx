39ba3e8b9701f825ab65b390b6a8c3c97de41750
Hubert Warchoł <n3tr00x99@gmail.com>
2025-05-28 14:50:53 +0200

    refactor: updated DialogRoot to always be open and controlled from the outside

--- src/components/NewEvent.tsx
+++ src/components/NewEvent.tsx
@@ -1,31 +1,20 @@
-import { ReactElement, RefAttributes, useEffect, useRef } from 'react';
+import { FormEvent, useEffect, useRef } from 'react';
 import { Controller, useForm, UseFormGetValues } from 'react-hook-form';
 import {
-	ButtonProps,
+	CloseButton,
 	createListCollection,
+	Dialog,
 	Flex,
 	Input,
+	Portal,
 	Textarea,
-	useDisclosure,
 	VStack,
 } from '@chakra-ui/react';
-import { MenuOpenChangeDetails } from '@chakra-ui/react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { AppwriteException } from 'appwrite';
 import { CaseSensitive, MapPin, MoveRight } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
-import {
-	DialogActionTrigger,
-	DialogBody,
-	DialogCloseTrigger,
-	DialogContent,
-	DialogFooter,
-	DialogHeader,
-	DialogRoot,
-	DialogTitle,
-	DialogTrigger,
-} from '@/components/ui/dialog';
 import { Field } from '@/components/ui/field';
 import { InputGroup } from '@/components/ui/input-group';
 import {
@@ -40,7 +29,8 @@ import { toaster } from '@/components/ui/toaster';
 import { useAddNewEvent, useEditEvent } from '@/hooks/appwrite';
 import { NewEventSchema } from '@/schemas/new-event.schema';
 import { useSelectedDate } from '@/store/date';
-import { Event, NewEventFormData } from '@/types/appwrite';
+import { useModal } from '@/store/modal';
+import { NewEventFormData } from '@/types/appwrite';
 import { addHoursAndResetMinutes, formatDateToYearMonthDay } from '@/utilities/date';
 
 export type DirtyField = {
@@ -76,15 +66,10 @@ const getEditedFields = (
 	}, {} as Partial<NewEventFormData>);
 };
 
-type NewEventModalProps = {
-	editedEvent?: Event;
-	dialogTriggerComponent: ReactElement<ButtonProps & RefAttributes<HTMLButtonElement>>;
-};
-
-export function NewEventModal({ dialogTriggerComponent, editedEvent }: NewEventModalProps) {
+export function NewEventModal() {
 	console.log('<NewEventModal /> render.');
 	const selectedDate = useSelectedDate();
-	const { open, onOpen, onClose, setOpen } = useDisclosure();
+	const { open, editedEvent, onEventFormClose, onSetEventFormState } = useModal();
 	const { mutateAsync: addNewEvent } = useAddNewEvent();
 	const { mutateAsync: editEvent } = useEditEvent();
 	const contentRef = useRef<HTMLDivElement>(null);
@@ -103,8 +88,12 @@ export function NewEventModal({ dialogTriggerComponent, editedEvent }: NewEventM
 		defaultValues: {
 			title: editedEvent ? editedEvent.title : '',
 			isAllDay: editedEvent ? editedEvent.isAllDay : false,
-			startDate: editedEvent ? editedEvent.startDate : formatDateToYearMonthDay(selectedDate),
-			endDate: editedEvent ? editedEvent.endDate : formatDateToYearMonthDay(selectedDate),
+			startDate: editedEvent
+				? formatDateToYearMonthDay(editedEvent.startDate)
+				: formatDateToYearMonthDay(selectedDate),
+			endDate: editedEvent
+				? formatDateToYearMonthDay(editedEvent.endDate)
+				: formatDateToYearMonthDay(selectedDate),
 			startTime: editedEvent ? editedEvent.startTime : addHoursAndResetMinutes(selectedDate),
 			endTime: editedEvent ? editedEvent.endTime : addHoursAndResetMinutes(selectedDate, 2),
 			location: editedEvent ? editedEvent.location : null,
@@ -113,32 +102,8 @@ export function NewEventModal({ dialogTriggerComponent, editedEvent }: NewEventM
 		},
 	});
 
-	const isEventModified = dirtyFields && Object.keys(dirtyFields).length === 0;
-
-	const toggleEventFormHandler = ({ open }: MenuOpenChangeDetails) => {
-		setOpen(open);
-		if (open && editedEvent) {
-			reset({
-				title: editedEvent.title,
-				isAllDay: editedEvent.isAllDay,
-				startDate: formatDateToYearMonthDay(editedEvent.startDate),
-				endDate: formatDateToYearMonthDay(editedEvent.endDate),
-				startTime: editedEvent.startTime,
-				endTime: editedEvent.endTime,
-				location: editedEvent.location,
-				description: editedEvent.description,
-				repeat: [editedEvent.repeat],
-			});
-		}
-	};
-
-	const isAllDaySelected = watch('isAllDay');
-	const startTime = watch('startTime');
-
-	useEffect(() => {
-		setValue('startDate', formatDateToYearMonthDay(selectedDate));
-		setValue('endDate', formatDateToYearMonthDay(selectedDate));
-	}, [selectedDate, setValue]);
+	const [isAllDaySelected, startTime] = watch(['isAllDay', 'startTime']);
+	const isEventModified = Object.keys(dirtyFields).length > 0;
 
 	useEffect(() => {
 		if (isAllDaySelected) {
@@ -163,169 +128,205 @@ export function NewEventModal({ dialogTriggerComponent, editedEvent }: NewEventM
 		}
 	}, [startTime, setValue]);
 
-	const editEventHandler = async () => {
-		onClose();
-		const modifiedFields = getEditedFields(dirtyFields, getValues);
-		try {
-			if (editedEvent) {
-				await editEvent({ eventId: editedEvent.$id, editedEvent: modifiedFields });
-
-				return toaster.create({
-					title: 'The event has been edited correctly!',
-					type: 'success',
-					placement: 'bottom-end',
-					duration: 4000,
-				});
-			}
-		} catch (error) {
-			if (error instanceof AppwriteException) {
-				return toaster.create({
-					title: 'An edit event problem',
-					type: 'error',
-					description: error.message,
-					placement: 'bottom-end',
-					duration: 4000,
-				});
-			}
-		} finally {
-			reset();
+	useEffect(() => {
+		if (editedEvent) {
+			reset({
+				title: editedEvent.title,
+				isAllDay: editedEvent.isAllDay,
+				startDate: formatDateToYearMonthDay(editedEvent.startDate),
+				endDate: formatDateToYearMonthDay(editedEvent.endDate),
+				startTime: editedEvent.startTime,
+				endTime: editedEvent.endTime,
+				location: editedEvent.location,
+				description: editedEvent.description,
+				repeat: [editedEvent.repeat],
+			});
+		} else {
+			reset({
+				title: '',
+				isAllDay: false,
+				startDate: formatDateToYearMonthDay(selectedDate),
+				endDate: formatDateToYearMonthDay(selectedDate),
+				startTime: addHoursAndResetMinutes(selectedDate),
+				endTime: addHoursAndResetMinutes(selectedDate, 2),
+				location: null,
+				description: null,
+				repeat: ['no-repeat'],
+			});
 		}
+	}, [editedEvent, selectedDate, reset]);
+
+	const editEventHandler = async () => {
+		onEventFormClose();
+
+		const modifiedFields = getEditedFields(dirtyFields, getValues);
+
+		if (editedEvent) {
+			toaster.promise(
+				editEvent.bind(null, { eventId: editedEvent.$id, editedEvent: modifiedFields }),
+				{
+					success: {
+						title: 'The event has been edited correctly!',
+					},
+					error: error => ({
+						title: 'An edit event problem',
+						description: error instanceof AppwriteException ? error.message : 'An unexpected error',
+					}),
+					loading: {
+						title: 'Editing event...',
+					},
+				},
+			);
+		}
+
+		reset();
 	};
 
-	const submitHandler = async (event: NewEventFormData) => {
-		onClose();
-		try {
-			await addNewEvent(event);
+	const addEventHandler = async (event: NewEventFormData) => {
+		onEventFormClose();
 
-			return toaster.create({
+		toaster.promise(addNewEvent.bind(null, event), {
+			success: {
 				title: 'The event has been added!',
-				type: 'success',
-				placement: 'bottom-end',
-				duration: 4000,
-			});
-		} catch (error) {
-			if (error instanceof AppwriteException) {
-				return toaster.create({
-					title: 'New event problem',
-					type: 'error',
-					description: error.message,
-					placement: 'bottom-end',
-					duration: 4000,
-				});
-			}
-		} finally {
-			reset();
+			},
+			error: error => ({
+				title: 'New event problem',
+				description: error instanceof AppwriteException ? error.message : 'An unexpected error',
+			}),
+			loading: {
+				title: 'Processing new event...',
+			},
+		});
+
+		reset();
+	};
+
+	const submitFormHandler = (event: FormEvent) => {
+		event.preventDefault();
+
+		if (editedEvent) {
+			handleSubmit(editEventHandler)(event);
+		} else {
+			handleSubmit(addEventHandler)(event);
 		}
 	};
 
 	return (
-		<DialogRoot size="xs" open={open} onOpenChange={toggleEventFormHandler}>
-			<DialogTrigger asChild onClick={onOpen}>
-				{dialogTriggerComponent}
-			</DialogTrigger>
-			<DialogContent ref={contentRef} zIndex={999}>
-				<form onSubmit={editedEvent ? handleSubmit(editEventHandler) : handleSubmit(submitHandler)}>
-					<DialogHeader>
-						<DialogTitle textAlign="center" fontFamily="heading">
-							{editedEvent ? 'Edit your event' : 'Add new event'}
-						</DialogTitle>
-					</DialogHeader>
-					<DialogBody>
-						<VStack gap={4}>
-							<Field invalid={!!errors.title} errorText={errors.title?.message}>
-								<InputGroup startElement={<CaseSensitive />} w="full">
-									<Input {...register('title')} ps={12} placeholder="Title" />
-								</InputGroup>
-							</Field>
-							<Field>
-								<Controller
-									name="isAllDay"
-									control={control}
-									render={({ field: { name, onChange, value } }) => (
-										<Switch
-											name={name}
-											checked={value}
-											onChange={onChange}
-											w="full"
-											colorPalette="blue"
-											justifyContent="space-between"
-											flexDirection="row-reverse"
-										>
-											All day
-										</Switch>
+		<Dialog.Root size="xs" open={open} onOpenChange={({ open }) => onSetEventFormState(open)}>
+			<Portal>
+				<Dialog.Backdrop />
+				<Dialog.Positioner>
+					<Dialog.Content onClick={event => event.stopPropagation()} ref={contentRef} zIndex={999}>
+						<form onSubmit={submitFormHandler}>
+							<Dialog.Header>
+								<Dialog.Title textAlign="center" fontFamily="heading">
+									{editedEvent ? 'Edit your event' : 'Add new event'}
+								</Dialog.Title>
+							</Dialog.Header>
+							<Dialog.Body>
+								<VStack gap={4}>
+									<Field invalid={!!errors.title} errorText={errors.title?.message}>
+										<InputGroup startElement={<CaseSensitive />} w="full">
+											<Input {...register('title')} ps={12} placeholder="Title" />
+										</InputGroup>
+									</Field>
+									<Field>
+										<Controller
+											name="isAllDay"
+											control={control}
+											render={({ field: { name, onChange, value } }) => (
+												<Switch
+													name={name}
+													checked={value}
+													onChange={onChange}
+													w="full"
+													colorPalette="blue"
+													justifyContent="space-between"
+													flexDirection="row-reverse"
+												>
+													All day
+												</Switch>
+											)}
+										/>
+									</Field>
+									<Field invalid={!!errors.endDate} errorText={errors.endDate?.message}>
+										<Flex flexDirection="row" alignItems="center" gap={8} maxW="full">
+											<Input {...register('startDate')} type="date" />
+											<MoveRight width={80} />
+											<Input {...register('endDate')} type="date" />
+										</Flex>
+									</Field>
+									{!isAllDaySelected && (
+										<Field invalid={!!errors.endTime} errorText={errors.endTime?.message}>
+											<Flex flexDirection="row" alignItems="center" gap={8} w="full">
+												<Input {...register('startTime')} type="time" />
+												<MoveRight width={80} />
+												<Input {...register('endTime')} type="time" />
+											</Flex>
+										</Field>
 									)}
-								/>
-							</Field>
-							<Field invalid={!!errors.endDate} errorText={errors.endDate?.message}>
-								<Flex flexDirection="row" alignItems="center" gap={8} maxW="full">
-									<Input {...register('startDate')} type="date" />
-									<MoveRight width={80} />
-									<Input {...register('endDate')} type="date" />
-								</Flex>
-							</Field>
-							{!isAllDaySelected && (
-								<Field invalid={!!errors.endTime} errorText={errors.endTime?.message}>
-									<Flex flexDirection="row" alignItems="center" gap={8} w="full">
-										<Input {...register('startTime')} type="time" />
-										<MoveRight width={80} />
-										<Input {...register('endTime')} type="time" />
-									</Flex>
-								</Field>
-							)}
-							<Field>
-								<Textarea minH={10} placeholder="Description" {...register('description')} />
-							</Field>
-							<Field>
-								<InputGroup startElement={<MapPin />} w="full">
-									<Input {...register('location')} ps={12} placeholder="Location" />
-								</InputGroup>
-							</Field>
-							<Field>
-								<Controller
-									name="repeat"
-									control={control}
-									render={({ field: { name, onChange, value } }) => (
-										<SelectRoot
-											name={name}
-											value={value}
-											onValueChange={({ value }) => onChange(value)}
-											collection={repeatFrequencyCollection}
-										>
-											<SelectTrigger>
-												<SelectValueText />
-											</SelectTrigger>
-											<SelectContent portalRef={contentRef}>
-												{repeatFrequencyCollection.items.map(item => (
-													<SelectItem item={item} key={item.value}>
-														{item.label}
-													</SelectItem>
-												))}
-											</SelectContent>
-										</SelectRoot>
-									)}
-								/>
-							</Field>
-						</VStack>
-					</DialogBody>
-					<DialogFooter>
-						<DialogActionTrigger asChild>
-							<Button variant="outline" onClick={event => event.stopPropagation()}>
-								Cancel
-							</Button>
-						</DialogActionTrigger>
-						<Button
-							type="submit"
-							colorPalette="blue"
-							minWidth={16}
-							disabled={isSubmitting || isEventModified}
-							onClick={event => event.stopPropagation()}
-						>
-							{editedEvent ? 'Edit' : 'Add'}
-						</Button>
-					</DialogFooter>
-					<DialogCloseTrigger />
-				</form>
-			</DialogContent>
-		</DialogRoot>
+									<Field>
+										<Textarea minH={10} placeholder="Description" {...register('description')} />
+									</Field>
+									<Field>
+										<InputGroup startElement={<MapPin />} w="full">
+											<Input {...register('location')} ps={12} placeholder="Location" />
+										</InputGroup>
+									</Field>
+									<Field>
+										<Controller
+											name="repeat"
+											control={control}
+											render={({ field: { name, onChange, value } }) => (
+												<SelectRoot
+													name={name}
+													value={value}
+													onValueChange={({ value }) => onChange(value)}
+													collection={repeatFrequencyCollection}
+												>
+													<SelectTrigger>
+														<SelectValueText />
+													</SelectTrigger>
+													<SelectContent portalRef={contentRef}>
+														{repeatFrequencyCollection.items.map(item => (
+															<SelectItem item={item} key={item.value}>
+																{item.label}
+															</SelectItem>
+														))}
+													</SelectContent>
+												</SelectRoot>
+											)}
+										/>
+									</Field>
+								</VStack>
+							</Dialog.Body>
+							<Dialog.Footer>
+								<Dialog.ActionTrigger asChild>
+									<Button
+										type="button"
+										variant="outline"
+										onClick={event => event.stopPropagation()}
+									>
+										Cancel
+									</Button>
+								</Dialog.ActionTrigger>
+								<Button
+									type="submit"
+									colorPalette="blue"
+									minWidth={16}
+									disabled={isSubmitting || !isEventModified}
+									onClick={event => event.stopPropagation()}
+								>
+									{editedEvent ? 'Edit' : 'Add'}
+								</Button>
+							</Dialog.Footer>
+							<Dialog.CloseTrigger asChild>
+								<CloseButton size="sm" />
+							</Dialog.CloseTrigger>
+						</form>
+					</Dialog.Content>
+				</Dialog.Positioner>
+			</Portal>
+		</Dialog.Root>
 	);
 }
